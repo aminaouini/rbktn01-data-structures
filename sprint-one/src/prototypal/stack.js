559f4d9aee437d2cbcf64d9118adbe36974a300f
aminaouini <aminaouini565@gmail.com>
2020-03-03 22:09:11 +0100

    prototypal

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
-var Stack = function() {
+var Stack = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+
+  obj.helper = 0;
+  obj.storage = {};
+
+  return obj
 };
 
 var stackMethods = {};
 
+stackMethods.push = function (value) {
+  this.storage[this.helper] = value;
+  this.helper++;
+};
+
+stackMethods.pop = function () {
+  if (this.helper) this.helper--;
+  const result = this.storage[this.helper];
+
+  return result;
+};
+
+stackMethods.size = function () {
+  return this.helper;
+};
 
