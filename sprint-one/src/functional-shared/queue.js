ca7bbeb78a2dbb9a71256d733a2ba0d2f9f544c2
aminaouini <aminaouini565@gmail.com>
2020-03-03 21:54:07 +0100

    functional-shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,41 @@
-var Queue = function() {
+var Queue = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = {};
+
+  _(someInstance).extend(queueMethods);
+
+
+  someInstance.storage = {};
+
+  someInstance.helper = 0;
+  someInstance.helper1 = 0;
+
+
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function (value) {
+  this.storage[this.helper] = value;
+  this.helper++;
+
+};
+
+queueMethods.dequeue = function () {
+  const result = this.storage[this.helper1];
+  this.helper1++;
+
+
+  return result;
+
+};
+
+queueMethods.size = function () {
+
+
+  return this.helper - this.helper1 >= 0 ? this.helper - this.helper1 : 0;
+};
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,34 @@
-var Stack = function() {
+var Stack = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+
+  _(obj).extend(stackMethods);
+
+  obj.storage = {};
+
+  obj.helper = 0;
+
+  return obj;
 };
 
 var stackMethods = {};
 
+
+// Implement the methods below
+stackMethods.push = function (value) {
+  this.storage[this.helper] = value;
+  this.helper++;
+};
+
+stackMethods.pop = function () {
+  if (this.helper) this.helper--;
+  const result = this.storage[this.helper];
+
+  return result;
+};
+
+stackMethods.size = function () {
+  return this.helper;
+};
 
