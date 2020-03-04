9c0d9ca65a408b2caacfa7c8e1f207a757d4475c
aminaouini <aminaouini565@gmail.com>
2020-03-04 09:50:32 +0100

    sprint1 is done

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
-var Queue = function() {
+var Queue = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.helper1 = 0;
+  this.helper = 0;
+  this.storage = {};
 };
 
 
+Queue.prototype.enqueue = function (value) {
+  this.storage[this.helper] = value;
+  this.helper++;
+
+};
+
+Queue.prototype.dequeue = function () {
+  const result = this.storage[this.helper1];
+  this.helper1++;
+
+
+  return result;
+
+};
+
+Queue.prototype.size = function () {
+
+
+  return this.helper - this.helper1 >= 0 ? this.helper - this.helper1 : 0;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,23 @@
-var Stack = function() {
+var Stack = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.helper = 0;
+  this.storage = {};
 };
 
 
+Stack.prototype.push = function (value) {
+  this.storage[this.helper] = value;
+  this.helper++;
+};
+
+Stack.prototype.pop = function () {
+  if (this.helper) this.helper--;
+  const result = this.storage[this.helper];
+
+  return result;
+};
+
+Stack.prototype.size = function () {
+  return this.helper;
+};
