d49f52e26e55cf59e009df9258105c26c589eb61
aminaouini <aminaouini565@gmail.com>
2020-03-05 19:35:36 +0100

    last test in searshBinaryTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,29 +1,68 @@
 var BinarySearchTree = function (value) {
-    var obj = Object.create(functionality);
+    let obj = Object.create(funcs);
 
-    return obj
+    obj.value = value;
+    obj.left = null;
+    obj.right = null;
+
+
+
+    return obj;
+
 };
 
-var functionality = {};
+var funcs = {};
 
-functionality.insert = function () {
+funcs.insert = function (value) {
+    console.log(this)
+    if (value < this.value) {
+        if (!this.left) {
+            this.left = BinarySearchTree(value);
+
+        } else {
+            this.left.insert(value)
+
+        }
+    }
+
+    if (this.value < value) {
+        if (!this.right) {
+            this.right = BinarySearchTree(value);
+
+        } else {
+            this.right.insert(value)
+
+        }
+
+    }
+
+}
+
+funcs.contains = function (target) {
+    if (this.value === target) return true;
+
+    else if (target < this.value && !!this.left) {
+
+        if (this.left.contains(target)) return true;
+
+    }
+
+    else if (this.value < target && !!this.right) {
+
+        if (this.right.contains(target)) return true;
+
+    }
+
+    return false;
+
+}
+
+funcs.depthFirstLog = function (cb) {
+
 
 
 }
 
-functionality.contains = function () {
-
-}
-
-functionality.depthFirstLog = function () {
-
-}
-
-var Node = function (value) {
-    this.value = value
-    this.right = null;
-    this.left = null;
-}
 
 /*
  * Complexity: What is the time complexity of the above functions?
